8f45203e59615bdb6d110193658d1c1567e9ecc9
0xFireball <0xFireball@outlook.com>
2018-03-11 23:02:34 -0700

    add a fire rate variable

--- src/simulation.h
+++ src/simulation.h
@@ -8,6 +8,7 @@ namespace simulation {
     constexpr double SCREEN_HEIGHT = 600;
     constexpr double PLAYER_SPEED = 2;
     constexpr double BULLET_SPEED = 4;
+    constexpr unsigned int FIRE_RATE = 2;
     
     double update(GameState& state, Action action);
     
