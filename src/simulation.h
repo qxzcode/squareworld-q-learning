d85f813f5325501dee47541088e7171c514ed9e6
qxzcode <qxzcraft@gmail.com>
2018-03-11 22:56:46 -0700

    Add some constants

--- src/simulation.h
+++ src/simulation.h
@@ -4,6 +4,11 @@
 
 namespace simulation {
     
+    constexpr double SCREEN_WIDTH = 800;
+    constexpr double SCREEN_HEIGHT = 600;
+    constexpr double PLAYER_SPEED = 2;
+    constexpr double BULLET_SPEED = 4;
+    
     double update(GameState& state, Action action);
     
 }
