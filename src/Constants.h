bf7cdf3daf70447bd7c6ae2e20b2eef4d844d997
0xFireball <0xFireball@outlook.com>
2018-03-13 10:00:03 -0700

    add an unused friction constant

--- src/Constants.h
+++ src/Constants.h
@@ -6,6 +6,7 @@ namespace simulation {
     constexpr double SCREEN_HEIGHT = 240;
     constexpr double PLAYER_ACCEL = 2;
     constexpr double PLAYER_MAX_SPEED = 2;
+    constexpr double PLAYER_FRICTION = 0.5;
     constexpr double BULLET_SPEED = 6;
     constexpr double PLAYER_SIZE = 6;
     constexpr unsigned int FIRE_RATE = 6;
