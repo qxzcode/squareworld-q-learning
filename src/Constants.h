893f7c93179269d5320e845b1c8d99fddd7d0c8f
0xFireball <0xFireball@outlook.com>
2018-03-13 08:29:45 -0700

    increase bullet speed further

--- src/Constants.h
+++ src/Constants.h
@@ -6,7 +6,7 @@ namespace simulation {
     constexpr double SCREEN_HEIGHT = 240;
     constexpr double PLAYER_ACCEL = 2;
     constexpr double PLAYER_MAX_SPEED = 2;
-    constexpr double BULLET_SPEED = 4;
+    constexpr double BULLET_SPEED = 6;
     constexpr double PLAYER_SIZE = 6;
-    constexpr unsigned int FIRE_RATE = 8;
+    constexpr unsigned int FIRE_RATE = 6;
 }
